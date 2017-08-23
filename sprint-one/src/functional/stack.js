0e3229b2a067a03eb100cbcf496a0f2113c928a0
KevinYingWeiYeh <yingweiyeh@my.smccd.edu>
2017-08-23 15:14:43 -0700

    complete stack portion

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -7,10 +7,23 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+//add count like 2 to 3
     storage.count++;
+//push value into someInstance
+    someInstance[storage.count] = value;
   };
 
   someInstance.pop = function() {
+//minus count like 3 to 2 
+//if count === 0 , do not minus
+    var result = someInstance[storage.count];
+    delete someInstance[storage.count];
+    if (storage.count > 0) {     
+      storage.count--;
+    }
+//delete the last value in someInstance
+//return the last value in someInstance
+    return result;
   };
 
   someInstance.size = function() {
