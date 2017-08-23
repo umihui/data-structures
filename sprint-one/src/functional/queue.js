9bae050db8c8cb8f3803e31a5b9e66cdf240a8fb
KevinYingWeiYeh <yingweiyeh@my.smccd.edu>
2017-08-23 16:40:35 -0700

    complete stack and queue in functional part

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -2,27 +2,25 @@ var Queue = function() {
   var someInstance = {};
   // Use an object with numeric keys to store values
   var storage = {};
-  //var keyArray = Object.keys(someInstance);
 
   // Implement the methods below
-  var count = 0;
+  
+ 
   someInstance.enqueue = function(value) {
-    count++;
-    storage[count] = value;
+    var keyArray = Object.keys(storage);
+    var newKey = keyArray[keyArray.length - 1] + 1;
+    storage[newKey] = value;
     
   };
 
   someInstance.dequeue = function() {
     var result = storage[Object.keys(storage)[0]]; 
-    if (count > 0) {
-      count--;
     
-    }
     delete storage[Object.keys(storage)[0]];
     return result;        
   };
   someInstance.size = function() {
-    return count;
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,31 +1,36 @@
 var Stack = function() {
   var someInstance = {};
   var storage = {};
-  var count = 0;
+  //var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
 //add count like 2 to 3
-    count++;
+    //count++;
 //push value into someInstance
-    storage[count] = value;
+    //storage[count] = value;
+    var key = Object.keys(storage).length + 1;
+    storage[key] = value;
   };
 
   someInstance.pop = function() {
 //minus count like 3 to 2 
 //if count === 0 , do not minus
-    var result = storage[count];
-    delete storage[count];
-    if (count > 0) {     
-      count--;
-    }
+    //var result = storage[count];
+    var last = Object.keys(storage)[Object.keys(storage).length - 1];
+    var result = storage[last];
+    delete storage[last];
+    // delete storage[count];
+    // if (count > 0) {     
+    //   count--;
+    // }
 //delete the last value in someInstance
 //return the last value in someInstance
     return result;
   };
 
   someInstance.size = function() {
-    return count;
+    return Object.keys(storage).length;
   };
 
   return someInstance;
