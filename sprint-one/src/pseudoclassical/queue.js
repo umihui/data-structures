c471f8910a71ed72a20c428543794981d50e538c
umihui <student@hackreactor.com>
2017-08-24 13:48:22 -0700

    complete sprint-one

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,24 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this;
+};
+
+Queue.prototype.enqueue = function (value) {
+  var lastIndex = Object.keys(this).length - 1;
+  var key = Object.keys(this)[lastIndex] + 1;
+  this[key] = value;
+};
+Queue.prototype.dequeue = function () {
+  var r = this[Object.keys(this)[0]];
+  //delete
+  delete this[Object.keys(this)[0]];
+  return r;
+};
+Queue.prototype.size = function () {
+  // body...
+  return Object.keys(this).length;
 };
 
 
+var any = new Queue();
