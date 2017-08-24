ddfca7301199d2307145825fb65e03204181b1f2
Hack Reactor Student <student@hackreactor.com>
2017-08-24 10:52:02 -0700

    complete stack portion in prototypal

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,34 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var obj = Object.create(stackMethods);
+
+  return obj;
 };
+
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  // body...
+  var key = Object.keys(this).length + 1;
+  this[key] = value;
 
+};
+
+stackMethods.pop = function () {
+  var keyArray = Object.keys(this);
+  if (keyArray.length === 0) {
+    return undefined;
+  } else {
+    var r = this[keyArray.length];
+    delete this[keyArray.length];
+    return r;
+  }
+  // body...
+};
+
+stackMethods.size = function() {
+  return Object.keys(this).length;
+
+};
